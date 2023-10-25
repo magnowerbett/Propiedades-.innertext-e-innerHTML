392f45bca14d65723a9afaf7dbf8946739757480
magnowerbett <magnowerbett@gmail.com>
2023-10-25 11:09:36 -0300

    ajustes01

--- script.js
+++ script.js
@@ -6,17 +6,14 @@ let link = document.querySelector('a');
 titulo.innerText = 'JavaScript - 1'
 link.innerText = 'Proz - Talento Cloud'
 
-// Capturando os elementos ul e ol
 let listaNaoOrdenada = document.querySelector('ul')
 let listaOrdenada = document.querySelector('ol')
 
-// Adicionando três itens simples na lista não ordenada
 listaNaoOrdenada.innerHTML = `
   <li>HTML</li>
   <li>CSS</li>
   <li>JavaScript</li>
 `
-// Adicionando três itens com links na lista ordenada
 listaOrdenada.innerHTML = `
   <li><a href="https:www.facebook.com">Facebook</a></li>
   <li><a href="https://www.amazon.com">Amazon</a></li>
